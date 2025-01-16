e1f798cda3cbe8c353270afafc8142f2df418310
Asror <asrorrustmaovich007@gmail.com>
2025-01-16 19:44:08 +0500

    third commit

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.getenv('DEBUG')))
+DEBUG = os.getenv('DEBUG', '0').lower() in ('1', 'true', 'yes')
 print(SECRET_KEY,DEBUG)
 
 ALLOWED_HOSTS = ["*"]
